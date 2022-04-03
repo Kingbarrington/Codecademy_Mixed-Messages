39f542f111e4f78d29aa648b62942d93b13591b8
kingbarrington <florislamfers@gmail.com>
2022-04-03 20:50:14 +0200

    finish project

--- script.js
+++ script.js
@@ -1,86 +1,102 @@
 const sadPoem1 = [
-  ["If I should die before the rest of you,"],
-  ["Break not a flower nor inscribe a stone."],
-  ["Nor, when I’m gone, speak in a Sunday voice,"],
-  ["But be the usual selves that I have known."]["Weep if you must,"],
-  ["Parting is hell."],
-  ["But life goes on,"],
-  ["So sing as well."],
+  "If I should die before the rest of you,",
+  "Break not a flower nor inscribe a stone.",
+  "Nor, when I’m gone, speak in a Sunday voice,",
+  "But be the usual selves that I have known.",
+  "Weep if you must,",
+  "Parting is hell.",
+  "But life goes on,",
+  "So sing as well.",
 ];
 
 const sadPoem2 = [
-  ["The rain drums down like red ants,"],
-  ["each bouncing off my window."],
-  ["The ants are in great pain"],
-  ["and they cry out as they hit"],
-  ["as if their little legs were only"],
-  ["stitched on and their heads pasted."],
-  ["And oh they bring to mind the grave,"],
-  ["so humble, so willing to be beat upon"],
-  ["with its awful lettering and"],
-  ["the body lying underneath"],
-  ["without an umbrella."],
-  ["Depression is boring, I think"],
-  ["and I would do better to make"],
-  ["some soup and light up the cave."],
+  "The rain drums down like red ants,",
+  "each bouncing off my window.",
+  "The ants are in great pain",
+  "and they cry out as they hit",
+  "as if their little legs were only",
+  "stitched on and their heads pasted.",
+  "And oh they bring to mind the grave,",
+  "so humble, so willing to be beat upon",
+  "with its awful lettering and",
+  "the body lying underneath",
+  "without an umbrella.",
+  "Depression is boring, I think",
+  "and I would do better to make",
+  "some soup and light up the cave.",
 ];
 
 const sadPoem3 = [
-  ["Age, and the deaths, and the ghosts."],
-  ["Her having gone away"],
-  ["in spirit from me. Hosts"],
-  ["of regrets come and find me empty."],
-  ["I don’t feel this will change."],
-  ["I don’t want anything"],
-  ["or person, familiar or strange."],
-  ["I don’t think I will sing"],
-  ["anymore just now,"],
-  ["or ever. I must start"],
-  ["to sit with a blind brow"],
-  ["above an empty heart."],
+  "Age, and the deaths, and the ghosts.",
+  "Her having gone away",
+  "in spirit from me. Hosts",
+  "of regrets come and find me empty.",
+  "I don’t feel this will change.",
+  "I don’t want anything",
+  "or person, familiar or strange.",
+  "I don’t think I will sing",
+  "anymore just now,",
+  "or ever. I must start",
+  "to sit with a blind brow",
+  "above an empty heart.",
 ];
 
 const kidPoem1 = [
-  ["Once there was an elephant,"],
-  ["Who tried to use the telephant"],
-  ["No! No! I mean an elephone"],
-  ["Who tried to use the telephone"],
-  ["Dear me! I am not certain quite"],
-  ["That even now Ive got it right."],
-  ["Howe’er it was, he got his trunk"],
-  ["Entangled in the telephunk"],
-  ["The more he tried to get it free,"],
-  ["The louder buzzed the telephee"],
-  ["I fear I’d better drop the song"],
-  ["Of elephop and telephong!"],
+  "Once there was an elephant,",
+  "Who tried to use the telephant",
+  "No! No! I mean an elephone",
+  "Who tried to use the telephone",
+  "Dear me! I am not certain quite",
+  "That even now Ive got it right.",
+  "Howe’er it was, he got his trunk",
+  "Entangled in the telephunk",
+  "The more he tried to get it free,",
+  "The louder buzzed the telephee",
+  "I fear I’d better drop the song",
+  "Of elephop and telephong!",
 ];
 
 const kidPoem2 = [
-  ["Hey diddle diddle,"],
-  ["The Cat and the fiddle,"],
-  ["The Cow jumped over the moon,"],
-  ["The little Dog laughed to see such sport,"],
-  ["And the Dish ran away with the Spoon."],
+  "Hey diddle diddle,",
+  "The Cat and the fiddle,",
+  "The Cow jumped over the moon,",
+  "The little Dog laughed to see such sport,",
+  "And the Dish ran away with the Spoon.",
 ];
 
 const kidPoem3 = [
-  ["As soon as Fred gets out of bed,"],
-  ["his underwear goes on his head."],
-  ["His mother laughs, “Don’t put it there,"],
-  ["a head’s no place for underwear!”"],
-  ["But near his ears, above his brains,"],
-  ["is where Fred’s underwear remains."],
-  ["At night when Fred goes back to bed,"],
-  ["he deftly plucks it off his head."],
-  ["His mother switches off the light"],
-  ["and softly croons, “Good night! Good night!”"],
-  ["And then, for reasons no one knows,"],
-  ["Fred’s underwear goes on his toes."],
+  "As soon as Fred gets out of bed,",
+  "his underwear goes on his head.",
+  "His mother laughs, “Don’t put it there,",
+  "a head’s no place for underwear!”",
+  "But near his ears, above his brains,",
+  "is where Fred’s underwear remains.",
+  "At night when Fred goes back to bed,",
+  "he deftly plucks it off his head.",
+  "His mother switches off the light",
+  "and softly croons, “Good night! Good night!”",
+  "And then, for reasons no one knows,",
+  "Fred’s underwear goes on his toes.",
 ];
 
-const poemArray = [[sadPoem1],[sadPoem2],[sadPoem3],[kidPoem1],[kidPoem2],[kidPoem3]];
+const poemArray = [
+  [sadPoem1],
+  [sadPoem2],
+  [sadPoem3],
+  [kidPoem1],
+  [kidPoem2],
+  [kidPoem3],
+];
 
-const randomTill = ceil => Math.floor((Math.random() * ceil)+1);
+const randomTill = (ceil) => Math.floor(Math.random() * ceil);
 
+const poemSelector = (arr) => {
+  const generatedPoem = [];
+  arr.forEach((poem) => {
+    poem.forEach(p => {
+      console.log(p[randomTill(p.length)]);
+    });
+  });
+};
 
-
+poemSelector(poemArray);
